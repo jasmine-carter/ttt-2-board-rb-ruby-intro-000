aa03dec5de9ef0423601be4db1c221e5bf1b466d
Jasmine Carter <jasmine.carter87@gmail.com>
2019-04-15 22:22:18 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,2 @@
 # Define the variable board below.
-board = [row_1, row_2, row_3]
-
-row_1 = [" ", " ", " "]
-row_2 = [" ", " ", " "]
-row_3 = [" ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
